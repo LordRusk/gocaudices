5332228da9173b7769cff5e89d3ca4222bf3d451
lordrusk <lordruskk@gmail.com>
2020-09-17 09:46:59 +0000

    gofmt completely on blocks.go

--- blocks.go
+++ blocks.go
@@ -9,12 +9,12 @@ var (
 
 	Blocks = []Block{
 		/* command */ /* update interval */ /* update sig */
-		Block{Cmd: "cat /tmp/recordingicon 2>/dev/null", UpInt: 0, UpSig: 9},
-		//Block { Cmd: 	"music", 	UpInt:		0,	UpSig:		11, },
+		// Block{Cmd: "cat /tmp/recordingicon 2>/dev/null", UpInt: 0, UpSig: 9},
+		// Block{Cmd: "music", UpInt: 0, UpSig: 11},
 		Block{Cmd: "pacpackages", UpInt: 0, UpSig: 8},
 		Block{Cmd: "news", UpInt: 0, UpSig: 6},
-		//Block { Cmd: "georona | cut -d' ' -f1,3", UpInt: 0,	UpSig:		19, },
-		//Block { Cmd: 	"crypto",	UpInt:		18000,	UpSig:		17, },
+		// Block{Cmd: "georona | cut -d' ' -f1,3", UpInt: 0, UpSig: 19},
+		// Block{Cmd: "crypto", UpInt: 18000, UpSig: 17},
 		Block{Cmd: "torrent", UpInt: 20, UpSig: 7},
 		Block{Cmd: "memory", UpInt: 10, UpSig: 14},
 		Block{Cmd: "cpu", UpInt: 3, UpSig: 13},
@@ -28,11 +28,11 @@ var (
 		Block{Cmd: "volume", UpInt: 0, UpSig: 10},
 		Block{Cmd: "battery", UpInt: 5, UpSig: 3},
 		Block{Cmd: "clock", UpInt: 0, UpSig: 1},
-		//Block { Cmd: 	"sip",		UpInt:		10,	UpSig:		2, },
-		//Block { Cmd: 	"vpnstat express", UpInt:	0,	UpSig:		21, },
+		// Block{Cmd: "sip", UpInt: 10, UpSig: 2},
+		// Block{Cmd: "vpnstat express", UpInt: 0, UpSig: 21},
 		Block{Cmd: "internet", UpInt: 5, UpSig: 4},
-		//Block { Cmd: 	"cord",		UpInt:		0,	UpSig:		23, },
-		//Block { Cmd: 	"help-icon",	UpInt:		0,	UpSig:		20, },
+		// Block{Cmd: "cord", UpInt: 0, UpSig: 23},
+		// Block{Cmd: "help-icon", UpInt: 0, UpSig: 20},
 	}
 )
 
