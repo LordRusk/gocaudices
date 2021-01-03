ab445937cde5176cc41177adf658375e91655e62
Prophet <54194338+LordRusk@users.noreply.github.com>
2021-01-03 00:44:29 -0800

    why have a variable outside of its scope of use?
    Many internal changes, hopefully ill stop continually changing little things here and there so perhaps paches can be made that will work longterm. I should also push a release soon.

--- main.go
+++ main.go
@@ -16,23 +16,21 @@ import (
 
 type block struct {
 	cmd   string
-	args  []string // used internally
 	inSh  bool
 	upInt int
 	upSig int
-	pos   int // used internally
+
+	args []string // used internally
+	pos  int      // used internally
 }
 
-var updateChan = make(chan interface{}, 1)
 var barBytesArr = make([][]byte, len(blocks))
-var sigChan = make(chan os.Signal, 1024)
-var signalMap = make(map[os.Signal][]block)
 
 func runBlock(b block) {
 	var outputBytes []byte
 	var err error
-	if b.args == nil {
-		outputBytes, err = exec.Command(b.cmd).Output()
+	if len(b.args) == 1 {
+		outputBytes, err = exec.Command(b.args[0]).Output()
 	} else {
 		outputBytes, err = exec.Command(b.args[0], b.args[1:]...).Output()
 	}
@@ -45,13 +43,17 @@ func runBlock(b block) {
 }
 
 func main() {
-	// setup X
+	// connect to X
 	x, err := xgb.NewConn()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Cannot connect to X! | %v\n", err)
 	}
 	defer x.Close()
 	root := xproto.Setup(x).DefaultScreen(x).Root
+
+	updateChan := make(chan interface{}, 1)
+	sigChan := make(chan os.Signal, 1024)
+	signalMap := make(map[os.Signal][]block)
 
 	for i := 0; i < len(blocks); i++ { // initialize blocks
 		blocks[i].pos = i
@@ -59,9 +61,7 @@ func main() {
 		if blocks[i].inSh {
 			blocks[i].args = []string{shell, cmdstropt, blocks[i].cmd}
 		} else {
-			if strings.Contains(blocks[i].cmd, " ") {
-				blocks[i].args = strings.Split(blocks[i].cmd, " ")
-			}
+			blocks[i].args = strings.Split(blocks[i].cmd, " ")
 		}
 
 		if blocks[i].upSig != 0 {
@@ -76,39 +76,39 @@ func main() {
 					time.Sleep(time.Duration(blocks[i].upInt) * time.Second)
 					runBlock(blocks[i])
 					select {
-					case updateChan <- nil:
-					default:
+						case updateChan <- nil:
+						default:
 					}
 				}
 			}(i)
 		}
 	}
-	updateChan <- nil:
 
-	go func() { // handle signals
-		for sig := range sigChan {
-			bs, _ := signalMap[sig]
-			for _, b := range bs {
-				runBlock(b)
+	go func() { // update bar on signal
+		var finalBytesBuffer bytes.Buffer
+		for range updateChan {
+			for _, b := range blocks {
+				if barBytesArr[b.pos] != nil {
+					finalBytesBuffer.Write(delim)
+					finalBytesBuffer.Write(barBytesArr[b.pos])
+				}
 			}
-			select {
-			case updateChan <- nil:
-			default:
-			}
+
+			finalBytes := bytes.TrimPrefix(finalBytesBuffer.Bytes(), delim)
+			xproto.ChangeProperty(x, xproto.PropModeReplace, root, xproto.AtomWmName, xproto.AtomString, 8, uint32(len(finalBytes)), finalBytes) // set the root window name
+			finalBytesBuffer.Reset()
 		}
 	}()
+	updateChan <- nil // initially update the bar
 
-	var finalBytesBuffer bytes.Buffer
-	for range updateChan { // update bar on signal
-		for _, b := range blocks {
-			if barBytesArr[b.pos] != nil {
-				finalBytesBuffer.Write(delim)
-				finalBytesBuffer.Write(barBytesArr[b.pos])
-			}
+	for sig := range sigChan { // handle signals
+		bs, _ := signalMap[sig]
+		for _, b := range bs {
+			runBlock(b)
 		}
-
-		finalBytes := bytes.TrimPrefix(finalBytesBuffer.Bytes(), delim)
-		xproto.ChangeProperty(x, xproto.PropModeReplace, root, xproto.AtomWmName, xproto.AtomString, 8, uint32(len(finalBytes)), finalBytes) // set the root window name
-		finalBytesBuffer.Reset()
+		select {
+		case updateChan <- nil:
+		default:
+		}
 	}
 }
